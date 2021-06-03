de695c6739a2af63492e78eefb3e266e63d50dd4
bloodyowl <bloodyowl@icloud.com>
2021-06-03 13:09:44 +0200

    Upstream changes

--- server/server.mjs
+++ server/server.mjs
@@ -3,195 +3,28 @@ import path from "path";
 import chalk from "chalk";
 import etag from "etag";
 import mime from "mime";
-import getPort from "get-port";
+import webpack from "webpack";
+
+import config from "../webpack.config.js";
 
 let fs = await import("fs");
+
+let { name } = JSON.parse(
+  fs.readFileSync(path.join(process.cwd(), "package.json"), "utf8")
+);
 
 let app = express();
 
 app.disable("x-powered-by");
 
-let pendingBuild = null;
-import webpack from "webpack";
-import config from "../webpack.config.js";
-import { createFsFromVolume, Volume } from "memfs";
-import WebSocket from "ws";
-let volume = new Volume();
-let outputFileSystem = createFsFromVolume(volume);
-let shouldRebuild = false;
-outputFileSystem.join = path.join.bind(path);
+import createRescriptDevserverTools from "rescript-devserver-tools";
 
-async function createWebsocketServer(port) {
-  let server = new WebSocket.Server({
-    port: port,
-  });
-  let openedConnections = [];
-  server.on("connection", (ws) => {
-    openedConnections.push(ws);
-    ws.on("close", () => {
-      openedConnections = openedConnections.filter((item) => item != ws);
-    });
-  });
-  return {
-    send: (message) => {
-      openedConnections.forEach((ws) => ws.send(message));
-    },
-  };
-}
+let { virtualFs, middleware, getLiveReloadAppendix } =
+  createRescriptDevserverTools(webpack(config));
 
-let reloadWsPort = await getPort();
-let reloadWs = await createWebsocketServer(reloadWsPort);
+fs = virtualFs;
 
-let suffix = `<script>new WebSocket("ws://localhost:${reloadWsPort}").onmessage = function() {location.reload(true)}</script>`;
-
-let compilers = [webpack(config)];
-
-let isFirstRun = true;
-
-function build() {
-  console.log(
-    chalk.white(new Date().toJSON()) + " " + chalk.blue("Webpack") + " start"
-  );
-  return (pendingBuild = Promise.all(
-    compilers.map((compiler) => {
-      return new Promise((resolve, reject) => {
-        compiler.outputFileSystem = outputFileSystem;
-        compiler.run((error, stats) => {
-          if (error) {
-            reject(error);
-          } else {
-            if (stats.hasErrors()) {
-              let errors = stats.toString("errors-warnings");
-              reject(errors);
-            } else {
-              if (!isFirstRun) {
-                reloadWs.send("change");
-              }
-              isFirstRun = false;
-              resolve();
-            }
-          }
-        });
-      });
-    })
-  )
-    .then(() => {
-      if (shouldRebuild) {
-        shouldRebuild = false;
-        return build();
-      } else {
-        console.log(
-          chalk.white(new Date().toJSON()) +
-            " " +
-            chalk.blue("Webpack") +
-            " done"
-        );
-        pendingBuild = null;
-      }
-    })
-    .catch((errors) => {
-      console.log(
-        chalk.white(new Date().toJSON()) +
-          " " +
-          chalk.blue("Webpack") +
-          " errored"
-      );
-      console.error(
-        "\n" +
-          errors
-            .split("\n")
-            .filter(
-              (line) => line !== "" && !line.startsWith("webpack compiled")
-            )
-            .map((line) => chalk.yellow(`    ${line}`))
-            .join("\n") +
-          "\n"
-      );
-      pendingBuild = null;
-    }));
-}
-
-process.nextTick(() => {
-  build();
-});
-
-function listenToReScript() {
-  let ws = new WebSocket("ws://localhost:9999");
-  let LAST_SEEN_SUCCESS_BUILD_STAMP = Date.now();
-
-  ws.on("open", () => {
-    console.log(
-      chalk.white(new Date().toJSON()) +
-        " " +
-        chalk.red("ReScript") +
-        " connected"
-    );
-  });
-
-  let hasErrored = false;
-  ws.on("error", () => {
-    hasErrored = true;
-    console.log(
-      chalk.white(new Date().toJSON()) +
-        " " +
-        chalk.red("ReScript") +
-        " failed to connect, retrying in 10s"
-    );
-    setTimeout(listenToReScript, 10000);
-  });
-
-  ws.on("message", (data) => {
-    let LAST_SUCCESS_BUILD_STAMP = JSON.parse(data).LAST_SUCCESS_BUILD_STAMP;
-    if (LAST_SUCCESS_BUILD_STAMP > LAST_SEEN_SUCCESS_BUILD_STAMP) {
-      console.log(
-        chalk.white(new Date().toJSON()) +
-          " " +
-          chalk.red("ReScript") +
-          " change"
-      );
-      LAST_SEEN_SUCCESS_BUILD_STAMP = LAST_SUCCESS_BUILD_STAMP;
-      if (pendingBuild == null) {
-        build();
-      } else {
-        shouldRebuild = true;
-      }
-    }
-  });
-
-  ws.on("close", () => {
-    if (!hasErrored) {
-      console.log(
-        chalk.white(new Date().toJSON()) +
-          " " +
-          chalk.red("ReScript") +
-          " disconnected, retrying in 10s"
-      );
-      setTimeout(listenToReScript, 10000);
-    }
-  });
-}
-
-listenToReScript();
-
-fs = outputFileSystem;
-
-// Delay requests until webpack has finished building
-app.use((req, res, next) => {
-  if (pendingBuild != null) {
-    pendingBuild.then(
-      () => {
-        process.nextTick(() => {
-          next();
-        }, 0);
-      },
-      () => {
-        res.status(500).end("Build error");
-      }
-    );
-  } else {
-    next();
-  }
-});
+app.use(middleware);
 
 let publicPath = process.env.PUBLIC_PATH || "/";
 
@@ -256,7 +89,7 @@ app.get(`${publicPath}*`, (req, res) => {
     path.join(process.cwd(), "build/index.html"),
     req,
     res,
-    suffix
+    getLiveReloadAppendix()
   );
 });
 
@@ -265,7 +98,7 @@ let port = process.env.PORT || 3000;
 app.listen(port);
 
 console.log(`${chalk.white("---")}`);
-console.log(`${chalk.green("ReScript React")}`);
+console.log(`${chalk.green(`${name}`)}`);
 console.log(`${chalk.white("---")}`);
 console.log(`${chalk.cyan("Development server started")}`);
 console.log(``);
